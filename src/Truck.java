a3871e6fb7246cd31c2086ad02f43919317970ca
Unique-Kris <titina91@mail.ru>
2024-08-10 13:50:32 +0300

    HW2.3 Исправленный код

--- src/Truck.java
+++ src/Truck.java
@@ -1,17 +1,21 @@
-public class Truck {
+public class Truck extends WheeledTransport {
 
-    public String modelName;
-    public int wheelsCount;
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+    public Truck(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
     }
 
-    public void checkEngine() {
+    public void checkEngineTruck() {
         System.out.println("Проверяем двигатель");
     }
 
-    public void checkTrailer() {
+    public static void checkTrailer() {
         System.out.println("Проверяем прицеп");
+    }
+
+    @Override
+    public void service() {
+        super.service();
+        checkEngineTruck();
+        checkTrailer();
     }
 }
